e343d57c27fdf9bea8ad416f50826e7bf761134f
apexa11 <apexachoxi7@gmail.com>
2017-04-05 17:47:01 -0400

    add chnages

--- rename_files.py
+++ rename_files.py
@@ -3,28 +3,39 @@ import os
 
 def rename_files():
 
-# get all file name from folder
+    # get all file name from folder
 
-    file_list = os.listdir(r"/Users/apexa/Desktop/Python Basic/img")
-    # r is stands for rawpack,it tells python "hey take this string as it is
-    #and dont interpret it any other way
-    print file_list
+        folder = os.listdir(r"/Users/apexa/Desktop/Python Basic/img")
 
-#cwd for current working directory
-save_path = os.getcwd()
+        # r is stands for rawpack,it tells python "hey take this string as it is
+        # and dont interpret it any other way
 
-print 'my current working directory' + save_path
-#chdir stands for change directory
-os.chdir(r"/Users/apexa/Desktop/Python Basic/img")
+        print folder
 
 
-  # rename all file from folder
+    # cwd for current working directory
 
-for file_name in file_list:
-    print("old name -"+file_name)
-    print("new name -"+file_name.translate(None, "0123456789"))
-    #rename the file list
-    os.rename(file_name, file_name.translate(None, "0123456789"))
-    #return to my original path
-    os.chdir(save_path)
-    rename_files()
+        save_path = os.getcwd()
+
+        print 'my current working directory' + save_path
+
+    # chdir stands for change directory
+
+        os.chdir(r"/Users/apexa/Desktop/Python Basic/img")
+
+      # rename all file from folder
+
+        for file_name in folder:
+            print 'old name -' + file_name
+            print 'new name -' + file_name.translate(None, '0123456789')
+
+            # rename the file list
+
+            os.rename(file_name, file_name.translate(None, '0123456789'))
+
+            # return to my original path
+
+        os.chdir(save_path)
+rename_files()
+
+			

73576fba06f801e090123f8bc4353da9636294ad
apexa11 <apexachoxi7@gmail.com>
2017-04-06 15:08:41 -0400

    design

--- circular_play.py
+++ circular_play.py
@@ -3,14 +3,14 @@ import turtle
 def draw_circle():
     pete = turtle.Turtle()
     pete.shape("turtle")
-    pete.color("white")
+    pete.color("red")
  
     for i in range(1,37):
      pete.circle(100)
      pete.left(10)
 
 window = turtle.Screen()
-window.bgcolor("black")
+window.bgcolor("yellow")
 draw_circle()
 window.exitonclick()
     
